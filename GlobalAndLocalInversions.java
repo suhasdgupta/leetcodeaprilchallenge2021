9a9264955299b8dd683bbca17b28c489e7febd6d
Suhaas D <suhasdgupta55555@gmail.com>
2021-04-13 17:15:07 +0530

    Create GlobalAndLocalInversions.java

---
+++ GlobalAndLocalInversions.java
@@ -0,0 +1,10 @@
+class GlobalAndLocalInversions {
+    public boolean isIdealPermutation(int[] nums) {
+        int cmax = 0;
+        for (int i = 0; i < nums.length - 2; ++i) {
+            cmax = Math.max(cmax, nums[i]);
+            if (cmax > nums[i + 2]) return false;
+        }
+        return true;
+    }
+}
